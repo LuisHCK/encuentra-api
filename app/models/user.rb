58f0c1372a8849eb024d5fa0e32c7632167a490c
Luis J. Centeno <luisjcenteno@live.com>
2018-08-06 16:51:16 -0600

    Use ActiveStorage instead CarrierWave

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  mount_uploader :avatar, AvatarUploader
+  # Now Using activestorage instead CarrierWave
+  # mount_uploader :avatar, AvatarUploader
+  has_one_attached :avatar
 
   # Basic password validation
   validates_length_of :password, maximum: 72, minimum: 8, allow_nil: true, allow_blank: false
